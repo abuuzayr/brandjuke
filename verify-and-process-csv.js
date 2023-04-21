eb8dd3a8c7d71b5c3391faeb9045e763cf6ddc20
Abu Uzayr <abu.uzayr@builtforfifty.com>
2023-04-21 17:07:24 +0800

    Update script

--- verify-and-process-csv.js
+++ verify-and-process-csv.js
@@ -83,15 +83,24 @@ async function uploadFile(imageUrl) {
     return imageUrl;
   }
 
-  return `${authObj["data"]["downloadUrl"]}/file/brandbuzza/${b64EncodedFileName}`;
+  // Get dominant color
+  await sharp(fileBuffer).toFile(path.join(__dirname, "/tmp.png"));
+  const color = await getColor(path.join(__dirname, "/tmp.png"));
+  const rgb = `#${color[0].toString(16).padStart(2, "0")}${color[1]
+    .toString(16)
+    .padStart(2, "0")}${color[2].toString(16).padStart(2, "0")}`;
+  const baseColorName = `${ntc.name(rgb)[3]}`.toLowerCase();
+
+  return {
+    url: `${authObj["data"]["downloadUrl"]}/file/brandbuzza/${b64EncodedFileName}`,
+    color: baseColorName
+  };
 }
 
 async function checkAndUploadImages() {
   const csvFile = "data/brands.csv";
-  const csvData = await fs.readFileSync(csvFile, "utf-8");
+  const csvData = fs.readFileSync(csvFile, "utf-8");
   const rows = parse(csvData, { header: true, skipEmptyLines: true }).data;
-
-  console.log(rows)
 
   for (const row of rows) {
     const imageUrl = row[imageColumn];
@@ -109,15 +118,81 @@ async function checkAndUploadImages() {
       continue;
     }
 
-    const uploadedUrl = await uploadFile(imageUrl);
+    const { url: uploadedUrl, color } = await uploadFile(imageUrl);
     if (uploadedUrl) {
       row[imageColumn] = uploadedUrl;
     }
+    row[colorColumn] = color;
   }
 
   const csvString = unparse(rows);
   fs.writeFileSync(csvFile, csvString);
   fs.appendFileSync(csvFile, "\r\n", "utf8");
+
+  const client = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+  });
+  const commits = await client.repos.listCommits({
+    // TODO: Change to brandbuzza-bot or something
+    owner: process.env.GITHUB_ACTOR,
+    repo: "brandbuzza",
+  });
+  const commitSHA = commits.data[0].sha;
+
+  const {
+    data: { sha: currentTreeSHA },
+  } = await client.git.createTree({
+    owner: process.env.GITHUB_ACTOR,
+    repo: "brandbuzza",
+    tree: [
+      {
+        path: "data/brands.csv",
+        mode: "100644",
+        type: "commit",
+        content: fs.readFileSync(csvFile, "utf-8"),
+      },
+    ],
+    base_tree: commitSHA,
+    message: `Update brands.csv by Github Action`,
+    parents: [commitSHA],
+  });
+
+  const {
+    data: { sha: newCommitSHA },
+  } = await client.git.createCommit({
+    owner: process.env.GITHUB_ACTOR,
+    repo: "brandbuzza",
+    tree: currentTreeSHA,
+    message: `Update brands.csv by Github Action`,
+    parents: [commitSHA],
+  });
+
+  const refHash = hasha(new Date().toISOString(), {
+    algorithm: "md5",
+  }).substring(0, 10);
+  const branchName = `update-brands-${refHash}`;
+  const refName = `heads/${branchName}`;
+
+  await client.git.createRef({
+    owner: process.env.GITHUB_ACTOR,
+    repo: "brandbuzza",
+    ref: `refs/${refName}`,
+    sha: newCommitSHA,
+  });
+
+  const createPrResponse = await client.rest.pulls.create({
+    owner: process.env.GITHUB_ACTOR,
+    repo: "brandbuzza",
+    head: branchName,
+    base: "main",
+    title: `Update brands.csv by Github Action`,
+  });
+
+  await client.rest.pulls.merge({
+    owner: process.env.GITHUB_ACTOR,
+    repo: "brandbuzza",
+    pull_number: createPrResponse.data.number,
+  });
 }
 
 checkAndUploadImages();
