b154837f6a06963e60bcbda5066d28340331fdcd
Abu Uzayr <abu.uzayr@builtforfifty.com>
2023-07-27 09:14:47 +0800

    Update brands API to handle new filters

--- pages/api/brands/index.ts
+++ pages/api/brands/index.ts
@@ -1,19 +1,41 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import duckdb from "duckdb";
+import { INDUSTRIES } from "@/lib/constants";
 
 const db = new duckdb.Database(":memory:");
 
 type BrandQuery = {
-    name?: string;
-}
+  name?: string;
+  colors?: string;
+  industries?: string;
+};
 
 const buildQuery = (filter: BrandQuery) => {
-  const { name } = filter;
+  const { name, colors, industries } = filter;
   let query = "SELECT * FROM read_csv_auto('data/brands.csv')";
   if (name) {
     query += ` WHERE name ILIKE '%${name}%'`;
   }
-  
+  if (colors) {
+    query += ` ${name ? "AND" : "WHERE"} color IN (${colors
+      .split(",")
+      .map((color) => `'${color}'`)
+      .join(",")})`;
+  }
+  if (industries) {
+    const industriesArr = industries
+      .split(",")
+      .map((i) => decodeURIComponent(i));
+    const industryKeys = industriesArr.map((i) =>
+      Object.values(INDUSTRIES).findIndex(
+        (e) => (e as string).toLowerCase() === i,
+      ),
+    );
+    query += ` ${
+      name || colors ? "AND" : "WHERE"
+    } industry IN (${industryKeys.join(",")})`;
+  }
+
   return query;
 };
 
@@ -30,7 +52,11 @@ const getBrands = (filter: BrandQuery) => {
 
 const create = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
-    const brands = await getBrands({ name: req.query?.search as string });
+    const brands = await getBrands({
+      name: req.query?.search as string,
+      colors: req.query?.colors as string,
+      industries: req.query?.industries as string,
+    });
     res.statusCode = 200;
     res.setHeader("Content-Type", "application/json");
     return res.status(200).json(brands);
