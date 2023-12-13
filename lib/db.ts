da0fa21a858f123822224fe36ea4682a6f136733
Abu Uzayr <abu.uzayr@builtforfifty.com>
2023-12-14 03:12:26 +0800

    refactor: not use create db

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,15 @@
 // lib/duckdb.ts
 let duckdb: any;
+let db: any
 
 try {
   // @ts-ignore
   duckdb = await import("duckdb-lambda-x86");
+  db = new duckdb.Database(":memory:")
 } catch (error) {
   console.log("duckdb init error:", error);
   duckdb = await import("duckdb-async");
+  db = await duckdb.Database.create(":memory:");
 }
 
-const db = await duckdb.Database.create(":memory:");
 export const _query = (query: string) => db.all(query)
