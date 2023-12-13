e5d915139d4569e614131584d82dcc28f8c5ffc8
Abu Uzayr <abu.uzayr@builtforfifty.com>
2023-12-14 03:05:13 +0800

    refactor: db query export

--- lib/db.ts
+++ lib/db.ts
@@ -1,25 +1,13 @@
 // lib/duckdb.ts
-let _query: Promise<(query: string) => any>;
+let duckdb: any;
 
-_query = import("duckdb-async")
-  .then((duckdb) => duckdb.Database)
-  .then((Database) => Database.create(":memory:"))
-  .then((db: any) => (query: string) => db.all(query))
-  .catch(async (error) => {
-    console.log("duckdb init error:", error);
-    // @ts-ignore
-    let duckdb = await import("duckdb-lambda-x86");
-    let Database: any = await duckdb.Database;
-    const db = new Database(":memory:");
-    const connection = db.connect();
-    return (query: string) => {
-      return new Promise((resolve, reject) => {
-        connection.all(query, (err: any, res: any) => {
-          if (err) reject(err);
-          resolve(res);
-        });
-      });
-    };
-  });
+try {
+  // @ts-ignore
+  duckdb = await import("duckdb-lambda-x86");
+} catch (error) {
+  console.log("duckdb init error:", error);
+  duckdb = await import("duckdb-async");
+}
 
-export { _query };
+const db = await duckdb.Database.create(":memory:");
+export const _query = (query: string) => db.all(query)
